e18afa54cfc42de3ea8559d04c4fe28db803cd1c
Emilie <adamanska@gmail.com>
2020-09-03 20:51:18 +0200

    wall slide part 2

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,6 +13,8 @@ export (float) var FRICTION = 0.25
 export (int) var GRAVITY = 200
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
+export (int) var SLIDE_SPEED = 48
+export (int) var MAX_SLIDE_SPEED = 128
 export (int) var BULLET_SPEED = 250
 
 enum{
@@ -64,7 +66,15 @@ func _physics_process(delta):
 			#transition to the wall slide
 			wall_slide_check()
 		WALL_SLIDE:
-			pass
+			spriteAnimator.play("Slide")
+			var wall_axis = get_wall_axis()
+			if wall_axis != 0:
+				sprite.scale.x = wall_axis
+			wall_slide_jump_check(wall_axis)
+			wall_slide_drop_check(delta)
+			wall_slide_fast_slide_check(delta)
+			move() # pour motion.x et y
+			wall_detach_check(wall_axis)
 		
 	if Input.is_action_pressed("Fire") and fireBulletTimer.time_left == 0:
 		fire_bullet()
@@ -180,3 +190,44 @@ func wall_slide_check():
 	if not is_on_floor() and is_on_wall():
 		state = WALL_SLIDE
 		dbl_jump = true
+		
+func get_wall_axis():
+	var is_right_wall = test_move(transform, Vector2.RIGHT)
+	# true si collision
+	var is_left_wall= test_move(transform, Vector2.LEFT)
+	return int(is_left_wall) - int(is_right_wall)
+	#-1 si mur à gauche, +1 à droite
+	
+func wall_slide_jump_check(wall_axis):
+	if Input.is_action_just_pressed("ui_up"):
+		motion.x = wall_axis * MAX_SPEED
+		motion.y = -JUMP_FORCE/1.25
+		state = MOVE
+		
+func wall_slide_drop_check(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		motion.x = ACCELERATION * delta
+		# delta non obligatoire
+		state = MOVE
+	if Input.is_action_just_pressed("ui_left"):
+		motion.x = -ACCELERATION * delta
+		state = MOVE
+	
+func wall_slide_fast_slide_check(delta):
+	var max_slide_speed = SLIDE_SPEED
+	if Input.is_action_just_pressed("ui_down"):
+		max_slide_speed = MAX_SLIDE_SPEED
+	motion.y = min (motion.y + GRAVITY * delta, max_slide_speed)
+
+func wall_detach_check(wall_axis):
+	if wall_axis == 0 or is_on_floor():
+		state = MOVE
+
+
+
+
+	
+	
+	
+	
+	
